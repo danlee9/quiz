bf7a69e936ce6bfbbced44636a25e404b139ac3d
unknown <dan.dh.lee@gmail.com>
2015-04-02 19:06:26 -0700

    Refactor code to incorporate Objects and replace placeholder text with actual questions and answers

--- js/app.js
+++ js/app.js
@@ -8,40 +8,42 @@ $(function() {
 	$answers.hide();
 
 	var count = 0,
-		correct = 0,
+		numCorrect = 0,
 		gameStart = false;
 	
 	var question, answers, answerVal, userVal;
 
-	var questionList = ['Question 1', 'Question 2', 'Question 3', 'Question 4', 
-	'Question 5'];
-
-	var answersList = [
-		["One", "Two", "Three", "Four"],
-		["ABC", "ADSF", "CHanges", "Test This OUT"],
-		["Something", "WOah", "Coolio", "Work IT"],
-		["WORK WORK", "More Work?", "Ye ye ye", "Hi there"],
-		["Testing", "Code", "more", "STUFF"]
+	var questionList = [
+		'In what city did the former NHL team, the Thrashers, play in?', 
+		'Which NFL team is the only one to go through an entire season undefeated?', 
+		'What was the best finish by a US team in the FIFA World Cup?', 
+		'Who holds the record for most NBA championships won as either a player or coach combined?', 
+		'Who was the first player to break Babe Ruth\'s former single season record of 60 home runs?'
 	];
 
-	var answerValList = [3, 2, 1, 1, 4];
+	var answersList = [
+		{choice: ["Kansas City, MO", "Charleston, SC", "Atlanta, GA", "San Diego, CA"], correct: 3},
+		{choice: ["1985 Chicago Bears", "1972 Miami Dolphins", "2007 New England Patriots", "1992 Dallas Cowboys"], correct: 2},
+		{choice: ["Third place", "Group Stage", "Quarter-finals", "Round of 16"], correct: 1},
+		{choice: ["Phil Jackson", "Bill Russell", "Pat Riley", "Steve Kerr"], correct: 1},
+		{choice: ["Barry Bonds", "Hank Aaron", "Mark McGwire", "Roger Maris"], correct: 4}
+	];
 
 
 	function updateQuestion() {
 		if (count !== 5) {
 			$('input[name="answer"]').attr('checked',false);
 			question = questionList[count];
-			answers = answersList[count];
+			answers = answersList[count].choice;
 			$h2.html(question);
 			$.each(answers, function(i, val) {
 				$label[i].innerHTML = val;
 			});
 		} else {
-			$h2.html('You answered ' + correct + ' out of 5 correct.' +
-			' Press "Try Again" to start over!');
+			$h2.html('Congrats on finishing the Quiz! You answered ' + numCorrect + ' out of 5 correct.' + ' Press "Try Again" to start over.');
 			$answers.hide();
 			$button.html("Try Again");
-			correct = 0;
+			numCorrect = 0;
 			count = 0;
 			gameStart = false;
 		}
@@ -49,28 +51,26 @@ $(function() {
 
 	function checkAnswer() {
 		if (gameStart === true && count !== 5) {
-			answerVal = answerValList[count];
+			answerVal = answersList[count].correct;
 			userVal = $('input[name="answer"]:checked').val();
 			if (answerVal == userVal) {
 				console.log("true");
 				$img[count].style.opacity = 1;
-				correct++;
+				numCorrect++;
 			} else {
 				console.log("false");
 			}
 			count++
-		}
-	}
-
-
-	$('button').on('click', function() {
-		checkAnswer();
-		if (gameStart === false) {
+		} else if (gameStart === false) {
 			$answers.show();
 			$button.html("Submit Answer");
 			$img.css("opacity", 0.1);
 			gameStart = true;
 		}
+	}
+
+	$('button').on('click', function() {
+		checkAnswer();
 		updateQuestion();
 	});
 })
